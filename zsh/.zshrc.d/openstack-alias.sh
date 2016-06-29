fa8155c9951f02a425a95bab04b416271c38ed5c
Gerard Braad <me@gbraad.nl>
2016-06-29 17:18:33 +0800

    Add 'openstack' alias

--- zsh/.zshrc.d/openstack-alias.sh
+++ zsh/.zshrc.d/openstack-alias.sh
@@ -1,2 +1,3 @@
 #!/bin/zsh
 alias stack='docker run -it --rm -v ~/.stack:/root/.stack gbraad/openstack-client:centos stack'
+alias openstack='docker run -it --rm gbraad/openstack-client:centos openstack'
