f6fb2411f442df6ec18c92bb3d9856eb44ea776e
Gerard Braad <me@gbraad.nl>
2016-07-10 16:16:01 +0800

    Add youtube-dl alias

--- zsh/.zshrc.d/alias.sh
+++ zsh/.zshrc.d/alias.sh
@@ -1,3 +1,7 @@
 #!/bin/zsh
+
+# OpenStack
 alias stack='docker run -it --rm -v ~/.stack:/root/.stack gbraad/openstack-client:centos stack'
 alias openstack='docker run -it --rm gbraad/openstack-client:centos openstack'
+
+alias yt='docker run --rm -u $(id -u):$(id -g) -v $PWD:/data vimagick/youtube-dl'
