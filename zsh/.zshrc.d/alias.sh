4396e3e1850370f2b54f76e4ee3d501979ef9c04
Gerard Braad <me@gbraad.nl>
2016-07-10 23:41:15 +0800

    Update alias.sh

--- zsh/.zshrc.d/alias.sh
+++ zsh/.zshrc.d/alias.sh
@@ -4,4 +4,4 @@
 alias stack='docker run -it --rm -v ~/.stack:/root/.stack gbraad/openstack-client:centos stack'
 alias openstack='docker run -it --rm gbraad/openstack-client:centos openstack'
 
-alias yt='docker run --rm -u $(id -u):$(id -g) -v $PWD:/data vimagick/youtube-dl'
+alias youtube-dl='docker run --rm -u $(id -u):$(id -g) -v $PWD:/data vimagick/youtube-dl'
