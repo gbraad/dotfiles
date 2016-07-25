6f43eca733ee69f46078b9367d7ef2e08185dcf6
Gerard Braad <me@gbraad.nl>
2016-07-25 10:15:12 +0800

    Update alias.sh

--- zsh/.zshrc.d/alias.sh
+++ zsh/.zshrc.d/alias.sh
@@ -1,7 +1,14 @@
 #!/bin/zsh
 
-# OpenStack
-alias stack='docker run -it --rm -v ~/.stack:/root/.stack gbraad/openstack-client:centos stack'
-alias openstack='docker run -it --rm gbraad/openstack-client:centos openstack'
+# OpenStack: https://gitlab.com/gbraad/openstack-client
+alias stack='docker run -it --rm -v ~/.stack:/root/.stack registry.gitlab.com/gbraad/openstack-client:centos stack'
+alias openstack='docker run -it --rm -v ~/.config/openstack:/root/.config/openstack registry.gitlab.com/gbraad/openstack-client:centos openstack'
 
+# devenv: https://gitlab.com/gbraad/devenv
+alias devenv='docker run -it --rm -v `pwd`:/workspace registry.gitlab.com/gbraad/devenv:f24'
+
+# pandoc: https://gitlab.com/gbraad/docugen
+alias pandoc='docker run -it --rm registry.gitlab.com/gbraad/docugen pandoc'
+
+# others
 alias youtube-dl='docker run --rm -u $(id -u):$(id -g) -v $PWD:/data vimagick/youtube-dl'
