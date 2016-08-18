f394f13f6ba0635d32a505a26ca6e41413cea5e5
Gerard Braad <me@gbraad.nl>
2016-08-18 17:21:07 +0800

    Add workdir to alias

--- zsh/.zshrc.d/alias.sh
+++ zsh/.zshrc.d/alias.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
 # OpenStack: https://gitlab.com/gbraad/openstack-client
-alias stack='docker run -it --rm -v ~/.stack:/root/.stack registry.gitlab.com/gbraad/openstack-client:centos stack'
-alias openstack='docker run -it --rm -v ~/.config/openstack:/root/.config/openstack registry.gitlab.com/gbraad/openstack-client:centos openstack'
+alias stack='docker run -it --rm -v $PWD:/workspace -v ~/.stack:/root/.stack registry.gitlab.com/gbraad/openstack-client:centos stack'
+alias openstack='docker run -it --rm -v $PWD:/workspace -v ~/.config/openstack:/root/.config/openstack registry.gitlab.com/gbraad/openstack-client:centos openstack'
 
 # Kubernetes
 alias kubectl='docker run -it --rm -v $PWD:/workspace registry.gitlab.com/gbraad/kubernetes-client:fedora kubectl'
@@ -17,4 +17,4 @@ alias pandoc='docker run -it --rm registry.gitlab.com/gbraad/docugen pandoc'
 alias youtube-dl='docker run --rm -u $(id -u):$(id -g) -v $PWD:/data vimagick/youtube-dl'
 
 # nginx
-alias nginx-root='docker run --name nginx-root -p 80:80 -v /var/srv:/usr/share/nginx/html:ro -d nginx'
+alias nginx-pwd='docker run --name nginx-pwd -p 80:80 -v $PWD:/usr/share/nginx/html:ro -d nginx'
