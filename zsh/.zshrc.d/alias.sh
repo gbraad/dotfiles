339bf93e5f5143ecd6a8f66a4ad4ccb5df4cfd3f
Gerard Braad <me@gbraad.nl>
2016-08-19 22:48:04 -0500

    Update alias.sh

--- zsh/.zshrc.d/alias.sh
+++ zsh/.zshrc.d/alias.sh
@@ -4,7 +4,7 @@
 alias stack='docker run -it --rm -v $PWD:/workspace -v ~/.stack:/root/.stack registry.gitlab.com/gbraad/openstack-client:centos stack'
 alias openstack='docker run -it --rm -v $PWD:/workspace -v ~/.config/openstack:/root/.config/openstack registry.gitlab.com/gbraad/openstack-client:centos openstack'
 
-# Kubernetes
+# Kubernetes: https://gitlab.com/gbraad/kubernetes-client
 alias kubectl='docker run -it --rm -v $PWD:/workspace registry.gitlab.com/gbraad/kubernetes-client:fedora kubectl'
 
 # devenv: https://gitlab.com/gbraad/devenv
@@ -13,11 +13,13 @@ alias devenv='docker run -it --rm -v $PWD:/workspace registry.gitlab.com/gbraad/
 # pandoc: https://gitlab.com/gbraad/docugen
 alias pandoc='docker run -it --rm -v $PWD:/workspace registry.gitlab.com/gbraad/docugen pandoc'
 
+# Mono / PowerShell: https://gitlab.com/gbraad/mono
+alias posh='docker run -it --rm -v $PWD:/workspace registry.gitlab.com/gbraad/mono:c7'
+
+# Python, etc
+# jupyter: https://gitlab.com/gbraad/jupyter
+alias jupyter='docker run -it --rm -v $PWD:/workspace -p 8888:8888 registry.gitlab.com/gbraad/jupyter:f24 jupyter'
+
 # others
 alias youtube-dl='docker run --rm -u $(id -u):$(id -g) -v $PWD:/data vimagick/youtube-dl'
-
-# nginx
 alias nginx-pwd='docker run --name nginx-pwd -p 80:80 -v $PWD:/usr/share/nginx/html:ro -d nginx'
-
-# Mono / PowerShell
-alias posh='docker run -it --rm -v $PWD:/workspace registry.gitlab.com/gbraad/mono:c7'
