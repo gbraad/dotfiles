104492198dbd99b31086485844fcdbae114bc29a
Gerard Braad <me@gbraad.nl>
2021-02-15 10:34:27 -0500

    Export display from WSL

--- zsh/.zshrc.d/wsl.sh
+++ zsh/.zshrc.d/wsl.sh
@@ -1,1 +1,5 @@
-export DONT_PROMPT_WSL_INSTALL=1
+#!/bin/sh
+if grep -qi Microsoft /proc/version; then
+   export DONT_PROMPT_WSL_INSTALL=1
+   DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
+fi
