908a365edbc698c67d3854a2526b3325ead8cd57
Gerard Braad <me@gbraad.nl>
2021-02-15 10:35:10 -0500

    Actually export

--- zsh/.zshrc.d/wsl.sh
+++ zsh/.zshrc.d/wsl.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if grep -qi Microsoft /proc/version; then
    export DONT_PROMPT_WSL_INSTALL=1
-   DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
+   export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
 fi
