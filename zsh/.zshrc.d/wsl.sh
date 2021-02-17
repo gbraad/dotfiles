f68186c4099a933528d9d1e7ff27ce4faf6fcb6e
Gerard Braad <me@gbraad.nl>
2021-02-17 15:13:18 +0800

    Force WSL to home folder
    Signed-off-by: Gerard Braad <me@gbraad.nl>

--- zsh/.zshrc.d/wsl.sh
+++ zsh/.zshrc.d/wsl.sh
@@ -2,4 +2,6 @@
 if grep -qi Microsoft /proc/version; then
    export DONT_PROMPT_WSL_INSTALL=1
    export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
+
+   cd ~
 fi
