a15c01ec3a076671a1053149e049cf7b07d0dea8
Gerard Braad <me@gbraad.nl>
2021-02-18 12:02:58 +0800

    WSL; added comments about environment variables

--- wsl/.zshrc.d/wsl.sh
+++ wsl/.zshrc.d/wsl.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+
+# prevent VScode startup message
 export DONT_PROMPT_WSL_INSTALL=1
+
+# allow remote X display
 export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
 
 # force to home directory!
