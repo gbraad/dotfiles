6f7ae6484851476bd9563b039de66676e589f572
Gerard Braad <me@gbraad.nl>
2021-02-19 12:44:08 +0800

    Update system install
    Signed-off-by: Gerard Braad <me@gbraad.nl>

--- zsh/.zshrc.d/system_install.sh
+++ zsh/.zshrc.d/system_install.sh
@@ -1,12 +1,18 @@
 if [ ! -z ${SYSTEM_INSTALL+x} ]; then
 
    # essentials
-   sudo dnf install -y mc tmux screen links lynx git-core stow vim
-   sudo dnf install -y powerline vim-powerline tmux-powerline
+   sudo dnf install -y \
+        mc tmux screen links lynx \
+        vim vim-nerdtree vim-airline \
+        powerline vim-powerline tmux-powerline
 
    # fonts
-   sudo dnf install -y adobe-source-han-mono-fonts adobe-source-code-pro-fonts adobe-source-sans-pro adobe-source-serif-pro-fonts
-   sudo dnf install -y powerline-fonts
+   sudo dnf install -y \
+        adobe-source-han-mono-fonts \
+        adobe-source-code-pro-fonts \
+        adobe-source-sans-pro \
+        adobe-source-serif-pro-fonts \
+        powerline-fonts
 
    # optiona
    sudo dnf install -y python3-pygments
