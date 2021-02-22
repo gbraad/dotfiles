3125403c4b809f2dd19c7320e06bc8940bb0b7f5
Gerard Braad <me@gbraad.nl>
2021-02-22 18:31:10 +0800

    Change order for powerline
    Signed-off-by: Gerard Braad <me@gbraad.nl>

--- zsh/.zshrc.d/powerline.sh
+++ zsh/.zshrc.d/powerline.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
-# use distro installed powerline
-if [ -f `which powerline-daemon` ]; then
+# check if powerline-local
+if [[ -r ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh ]]; then
+    source ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
+# else use distro installed powerline
+elif [[ -f `which powerline-daemon` ]]; then
     powerline-daemon -q
     POWERLINE_ZSH_CONTINUATION=1
     POWERLINE_ZSH_SELECT=1
     . /usr/share/powerline/zsh/powerline.zsh
-# else check if powerline-local
-elif [[ -r ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh ]]; then
-    source ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
 fi
 
