6aa8f4b3f48f23cf1393dbd489cf016d339d2d78
Gerard Braad <me@gbraad.nl>
2021-02-22 18:19:00 +0800

    Change powerline implementation
    Signed-off-by: Gerard Braad <me@gbraad.nl>

--- zsh/.zshrc.d/powerline.sh
+++ zsh/.zshrc.d/powerline.sh
@@ -1,4 +1,12 @@
 #!/bin/zsh
-if [[ -r ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh ]]; then
+# use distro installed powerline
+if [ -f `which powerline-daemon` ]; then
+    powerline-daemon -q
+    POWERLINE_ZSH_CONTINUATION=1
+    POWERLINE_ZSH_SELECT=1
+    . /usr/share/powerline/zsh/powerline.zsh
+# else check if powerline-local
+elif [[ -r ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh ]]; then
     source ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
 fi
+
