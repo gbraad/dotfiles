06ebbb9394c21de7257374f8a51d1f14cfa2acee
Gerard Braad <me@gbraad.nl>
2023-04-21 12:36:18 +0000

    taildock (wip)

--- zsh/.zshrc.d/tailscale.zsh
+++ zsh/.zshrc.d/tailscale.zsh
@@ -1,8 +1,16 @@
 #!/bin/zsh
+
 # tailscale
 alias ts='tailscale'
 alias tsh='tailscale ssh'
+
 if [[ $(uname) == "Darwin" ]]; then
     alias tailscale='/Applications/Tailscale.app//Contents/MacOS/Tailscale'
 fi
+
+# tailproxy (user mode instance)
 alias tailproxy=". ~/.local/bin/start-tailproxy"
+
+# container
+alias taildock='podman run -d --name=tailscaled -v /var/lib:/var/lib -v /dev/net/tun:/dev/net/tun --network=host --cap-add=NET_ADMIN --cap-add=NET_RAW --env TS_AUTHKEY=$TS_AUTHKEY tailscale/tailscale'
+
