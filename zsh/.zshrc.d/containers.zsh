63506172fbf321bb920fe1cbf30865ec4b986b93
Gerard Braad <me@gbraad.nl>
2023-05-07 01:00:21 +0800

    Use devenv based on host distro

--- zsh/.zshrc.d/containers.zsh
+++ zsh/.zshrc.d/containers.zsh
@@ -18,29 +18,7 @@ alias steamcmd='${CONTAINER_RUNTIME} run -it -v $HOME/Steam:/home/user/Steam reg
 # hostenter
 alias hostenter='${CONTAINER_RUNTIME} run --rm -it --privileged --pid=host gbraad/hostenter /bin/bash'
 
-# --- Fedora devenv --- https://github.com/gbraad-devenv/fedora
-
-# dotfiles
-alias devenv='${CONTAINER_RUNTIME} run -it --cap-add=NET_ADMIN --cap-add=NET_RAW --device=/dev/net/tun --rm -v $HOME/Projects:/home/${USER}/Projects ghcr.io/gbraad-devenv/fedora/dotfiles:37 /bin/zsh'
-
-# systemd
-alias devsys='podman run -d --name=devsys --hostname $HOSTNAME-devsys --systemd=always --cap-add=NET_ADMIN --cap-add=NET_RAW --device=/dev/net/tun -v $HOME/Projects:/home/${USER}/Projects ghcr.io/gbraad-devenv/fedora/systemd:37'
-alias devdock='docker run -d --name=devsys --hostname $HOSTNAME-devsys --cap-add=NET_ADMIN --cap-add=NET_RAW --device=/dev/net/tun -v $HOME/Projects:/home/${USER}/Projects --entrypoint="" ghcr.io/gbraad-devenv/fedora/dotfiles:37 sleep infinity'
-alias devroot='${CONTAINER_RUNTIME} exec -it devsys /bin/zsh'
-alias devuser='${CONTAINER_RUNTIME} exec -it devsys su - gbraad'
-
-# ---
-
 # fedora coreos
 alias fcos='podman run -d --name=fcos --hostname $HOSTNAME-fcos --systemd=always --cap-add=NET_ADMIN --cap-add=NET_RAW --device=/dev/net/tun quay.io/fedora/fedora-coreos:stable /sbin/init'
 alias fcosroot='${CONTAINER_RUNTIME} exec -it fcos /bin/bash'
 
-# --- Debian devenv --- https://github.com/gbraad-devenv/debian/
-
-# dotfiles
-alias debenv='${CONTAINER_RUNTIME} run -it --cap-add=NET_ADMIN --cap-add=NET_RAW --device=/dev/net/tun --rm -v $HOME/Projects:/home/${USER}/Projects --entrypoint="" ghcr.io/gbraad-devenv/debian/dotfiles:bullseye /bin/zsh'
-
-# systemd
-alias debsys='podman run -d --name=debsys --hostname $HOSTNAME-debsys --systemd=always --cap-add=NET_ADMIN --cap-add=NET_RAW --device=/dev/net/tun -v $HOME/Projects:/home/${USER}/Projects ghcr.io/gbraad-devenv/debian/systemd:bullseye'
-alias debroot='${CONTAINER_RUNTIME} exec -it debsys /bin/zsh'
-alias debuser='${CONTAINER_RUNTIME} exec -it debsys su - gbraad'
---
+++ zsh/.zshrc.d/devenv.zsh
@@ -0,0 +1,45 @@
+#!/bin/zsh
+CONTAINER_RUNTIME="${1:-podman}"
+
+
+# --- Fedora devenv --- https://github.com/gbraad-devenv/fedora
+
+# dotfiles
+alias defenv='${CONTAINER_RUNTIME} run -it --cap-add=NET_ADMIN --cap-add=NET_RAW --device=/dev/net/tun --rm -v $HOME/Projects:/home/${USER}/Projects ghcr.io/gbraad-devenv/fedora/dotfiles:37 /bin/zsh'
+
+# systemd
+alias defsys='podman run -d --name=devsys --hostname $HOSTNAME-devsys --systemd=always --cap-add=NET_ADMIN --cap-add=NET_RAW --device=/dev/net/tun -v $HOME/Projects:/home/${USER}/Projects ghcr.io/gbraad-devenv/fedora/systemd:37'
+alias defdock='docker run -d --name=devsys --hostname $HOSTNAME-devsys --cap-add=NET_ADMIN --cap-add=NET_RAW --device=/dev/net/tun -v $HOME/Projects:/home/${USER}/Projects --entrypoint="" ghcr.io/gbraad-devenv/fedora/dotfiles:37 sleep infinity'
+alias defroot='${CONTAINER_RUNTIME} exec -it devsys /bin/zsh'
+alias defuser='${CONTAINER_RUNTIME} exec -it devsys su - gbraad'
+
+
+# --- Debian devenv --- https://github.com/gbraad-devenv/debian/
+
+# dotfiles
+alias debenv='${CONTAINER_RUNTIME} run -it --cap-add=NET_ADMIN --cap-add=NET_RAW --device=/dev/net/tun --rm -v $HOME/Projects:/home/${USER}/Projects --entrypoint="" ghcr.io/gbraad-devenv/debian/dotfiles:bullseye /bin/zsh'
+
+# systemd
+alias debsys='podman run -d --name=debsys --hostname $HOSTNAME-debsys --systemd=always --cap-add=NET_ADMIN --cap-add=NET_RAW --device=/dev/net/tun -v $HOME/Projects:/home/${USER}/Projects ghcr.io/gbraad-devenv/debian/systemd:bullseye'
+alias debroot='${CONTAINER_RUNTIME} exec -it debsys /bin/zsh'
+alias debuser='${CONTAINER_RUNTIME} exec -it debsys su - gbraad'
+
+
+# --- base on host distro
+
+source /etc/os-release
+
+if [ "$ID" = "fedora" ]
+then
+    alias devenv=defenv
+    alias devsys=defsys
+    alias devroot=defroot
+    alias devuser=defuser
+elif [ "$ID" = "debian" ]
+then
+    alias devenv=debenv
+    alias devsys=debsys
+    alias devroot=debroot
+    alias devuser=debuser
+fi
+
