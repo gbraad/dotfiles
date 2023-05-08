3b7a7b5db203c81f78ffa3e08200cd1ac6405377
Gerard Braad <me@gbraad.nl>
2023-05-09 00:11:22 +0800

    Update tailscale.zsh

--- zsh/.zshrc.d/tailscale.zsh
+++ zsh/.zshrc.d/tailscale.zsh
@@ -16,5 +16,5 @@ fi
 alias tailproxy=". ~/.local/bin/start-tailproxy"
 
 # container
-alias taildock='podman run -d --name=tailscaled -v /var/lib:/var/lib -v /dev/net/tun:/dev/net/tun --network=host --cap-add=NET_ADMIN --cap-add=NET_RAW --env TS_AUTHKEY=$TS_AUTHKEY tailscale/tailscale'
-alias tailwings='podman run -d --name=tailwings --env TAILSCALE_AUTH_KEY=$TS_AUTHKEY --cap-add=NET_ADMIN --cap-add=NET_RAW --device=/dev/net/tun ghcr.io/spotsnel/tailscale-tailwings:latest'
+alias taildock='podman run -d --name=tailscaled -v /var/lib:/var/lib -v /dev/net/tun:/dev/net/tun --network=host --cap-add=NET_ADMIN --cap-add=NET_RAW --env TS_AUTHKEY=$TAILSCALE_AUTHKEY tailscale/tailscale'
+alias tailwings='podman run -d --name=tailwings --env TAILSCALE_AUTH_KEY=$TAILSCALE_AUTHKEY --cap-add=NET_ADMIN --cap-add=NET_RAW --device=/dev/net/tun ghcr.io/spotsnel/tailscale-tailwings:latest'
