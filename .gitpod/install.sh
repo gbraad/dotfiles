de32f97b7bdad6677a9119615ee302fbb7505230
Gerard Braad <me@gbraad.nl>
2023-05-09 06:26:51 +0000

    Update pivot script

--- .gitpod/install.sh
+++ .gitpod/install.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 rm -rf ~/.dotfiles
-ln -s /workspace/dotfiles ~/.dotfiles
+mv /workspace/dotfiles ~/.dotfiles
+rm -rf /workspace/dotfiles
+ln -s ~/.dotfiles /workspace/dotfiles
 cd ~/.dotfiles
+
+git fetch --unshallow
 git submodule update --init --progress
+
+rm -f ~/.zshrc
+stow -R zsh
 
 exit 0
