9dd521841ffbc90279c0bd90fe52eb5bdfafe4a2
Gerard Braad <me@gbraad.nl>
2024-12-27 19:42:25 +0800

    Simplify devenv aliases

--- zsh/.zshrc.d/devenv.zsh
+++ zsh/.zshrc.d/devenv.zsh
@@ -1,81 +1,53 @@
 #!/bin/zsh
-CONTAINER_RUNTIME="${1:-podman}"
 
-# Common function to generate aliases
 generate_aliases() {
   local PREFIX=$1
   local NAME=$2
   local VERSION=$3
   local EXTRA_ARGS=$4
 
-  # Combined capabilities and devices
   local START_ARGS="\
-    --cap-add=NET_ADMIN \
-    --cap-add=NET_RAW \
-    --device=/dev/net/tun \
     --systemd=always \
+    --cap-add=NET_RAW \
+    --cap-add=NET_ADMIN \
     --cap-add=SYS_ADMIN \
+    --device=/dev/net/tun \
     --device=/dev/fuse \
   "
 
-  alias ${PREFIX}env="${CONTAINER_RUNTIME} run -it ${START_ARGS} --rm -v ${HOME}/Projects:/home/${USER}/Projects --entrypoint='' ghcr.io/gbraad-devenv/${NAME}/dotfiles:${VERSION} ${EXTRA_ARGS} zsh"
-  alias ${PREFIX}sys="${CONTAINER_RUNTIME} run -d --name=${PREFIX}sys --hostname ${HOSTNAME}-${PREFIX}sys ${START_ARGS} -v ${HOME}/Projects:/home/${USER}/Projects ghcr.io/gbraad-devenv/${NAME}/systemd:${VERSION} \
+  alias ${PREFIX}env="podman run -it ${START_ARGS} --rm -v ${HOME}/Projects:/home/${USER}/Projects --entrypoint='' ghcr.io/gbraad-devenv/${NAME}/dotfiles:${VERSION} ${EXTRA_ARGS} zsh"
+  alias ${PREFIX}sys="podman run -d --name=${PREFIX}sys --hostname ${HOSTNAME}-${PREFIX}sys ${START_ARGS} -v ${HOME}/Projects:/home/${USER}/Projects ghcr.io/gbraad-devenv/${NAME}/systemd:${VERSION} \
      && (mkdir -p ${HOME}/.config/systemd/user && cd ${HOME}/.config/systemd/user \
-     && ${CONTAINER_RUNTIME} generate systemd --name --files ${PREFIX}sys) \
+     && podman generate systemd --name --files ${PREFIX}sys) \
      && systemctl --user daemon-reload \
      && systemctl --user start container-${PREFIX}sys"
-  alias ${PREFIX}root="${CONTAINER_RUNTIME} exec -it ${PREFIX}sys /bin/zsh"
-  alias ${PREFIX}user="${CONTAINER_RUNTIME} exec -it ${PREFIX}sys su - gbraad"
+  alias ${PREFIX}root="podman exec -it ${PREFIX}sys /bin/zsh"
+  alias ${PREFIX}user="podman exec -it ${PREFIX}sys su - gbraad"
 
-  generate_control_aliases "${PREFIX}"
-}
-
-# Function to generate common control aliases
-generate_control_aliases() {
-  local PREFIX=$1
   alias ${PREFIX}start="systemctl --user start container-${PREFIX}sys"
-  alias ${PREFIX}stop="systemctl --user stop container-${PREFIX}sys && ${CONTAINER_RUNTIME} stop ${PREFIX}sys"
-  alias ${PREFIX}exec="${CONTAINER_RUNTIME} exec -it ${PREFIX}sys"
+  alias ${PREFIX}stop="systemctl --user stop container-${PREFIX}sys"
+    # && ${CONTAINER_RUNTIME} stop ${PREFIX}sys"
+  alias ${PREFIX}exec="podman exec -it ${PREFIX}sys"
   alias ${PREFIX}sysctl="${PREFIX}exec systemctl"
   alias ${PREFIX}status="${PREFIX}sysctl status"
   alias ${PREFIX}tmux="${PREFIX}user -c 'tmux -2'"
 }
 
-# Fedora devenv
 FEDORA_VERSION="41"
-#generate_aliases "def" "fedora" ${FEDORA_VERSION} ""
 generate_aliases "fed" "fedora" ${FEDORA_VERSION} ""
-
-# Debian devenv
+#generate_aliases "def" "fedora" ${FEDORA_VERSION} ""
 generate_aliases "deb" "debian" "bookworm" ""
-
-# Alpine devenv
 generate_aliases "alp" "alpine" "3.18" ""
-
-# CentOS devenv
 generate_aliases "cen" "centos" "stream9" ""
-
-# UBI9 Go toolset devenv
 generate_aliases "go" "ubi9-gotoolset" "1.22.7" ""
-
-# UBI devenv
 generate_aliases "ubi" "ubi" "9" ""
-
-# Ubuntu devenv
 generate_aliases "ubu" "ubuntu" "jammy" ""
-
-# AlmaLinux devenv
 generate_aliases "alm" "almalinux" "9" ""
-
-# OpenSUSE devenv
 generate_aliases "sus" "opensuse" "15.5" ""
-
-# Tumbleweed devenv
 generate_aliases "tum" "tumbleweed" "latest" ""
 
 # Base on host distro
 source /etc/os-release
-
 case "$ID" in
     "fedora" | "bazzite")
         alias devenv="fedenv"
