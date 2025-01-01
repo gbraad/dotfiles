3d1d20a136889a9d509cddfd1eac871b1a2eeb7a
Gerard Braad <me@gbraad.nl>
2025-01-01 13:20:12 +0800

    Initial screen wrapper

---
+++ zsh/.zshrc.d/screen.zsh
@@ -0,0 +1,38 @@
+#!/bin/zsh
+
+CONFIG="${HOME}/.screenrc"
+alias screenini="git config -f ${CONFIG}"
+
+screen () {
+  local OVERRIDE=$(screenini --bool "screen.override")
+  if [[ ${OVERRIDE} == true ]]; then
+    local screenname="screen"
+
+    if [[ -n "${TMUX}" ]]; then
+      tmux split-window -h "$@"
+
+    else 
+     
+      tmux has-session -t ${screenname} 2>/dev/null
+      if [[ $? != 0 ]]; then
+        tmux new-session -d -s ${screenname}
+        tmux send-keys -t ${screenname} $@ C-m
+        tmux attach-session -t ${screenname}
+      else
+        tmux attach-session -t ${screenname}
+      fi
+    fi
+
+  else
+    local distscreen="/usr/bin/screen"
+    local brewscreen="/var/home/linuxbrew/.linuxbrew/bin/screen"
+
+    if [[ -e "${distscreen}" ]]; then
+      ${distscreen} $@
+    elif [[ -e "${brewscreen}" ]]; then
+      ${brewscreen} $@
+    else
+      echo "screen not found: please use override or tmux"
+    fi
+  fi
+}
