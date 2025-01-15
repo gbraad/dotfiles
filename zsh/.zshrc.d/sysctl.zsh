2ec39447ba92e1fef759c159cae3e8e08a0e40c7
Gerard Braad <me@gbraad.nl>
2025-01-15 10:35:09 +0800

    Tailsysctl

--- zsh/.zshrc.d/sysctl.zsh
+++ zsh/.zshrc.d/sysctl.zsh
@@ -12,3 +12,17 @@ podsysctl() {
    podman exec -it ${name} systemctl $@
 
 }
+
+tailsysctl() {
+
+   if [ $# -lt 2 ]; then
+       echo "Usage: tailsysctl <name> <command> [args ...]"
+       return 1
+   fi
+
+   name=$1
+   shift 1
+   tailscale ssh ${USER}@${name} systemctl $@
+
+}
+
