ea2b3288667ab31a94db3b6650becfcd1e33f007
Gerard Braad <me@gbraad.nl>
2025-01-25 23:41:18 +0800

    Clear and fuzzy select proxy

--- zsh/.zshrc.d/proxy.zsh
+++ zsh/.zshrc.d/proxy.zsh
@@ -3,18 +3,31 @@
 CONFIG="${HOME}/.proxy"
 alias proxyini="git config -f $CONFIG"
 
+proxyselect() {
+  echo $(proxyini --list | grep '^servers\.' | sed 's/^servers\.//g' | cut -d '=' -f 1 | fzf)
+}
+
 proxy() {
-  if [ $# -lt 1 ]; then
-    echo "Usage: $0 <prefix> [args...]"
-    return 1
-  fi
 
   local PREFIX=$1
+
+  if [ -z "$PREFIX" ]; then
+    PREFIX=$(proxyselect)
+  fi
+
+  if [ -z "$PREFIX" ]; then
+    echo "Clearing proxy setting"
+    unset http_proxy
+    unset https_proxy
+    return
+  fi
+
   local SERVER=$(proxyini --get servers.${PREFIX})
 
   export http_proxy=${SERVER}
   export https_proxy=${SERVER}
 
+  echo "Proxy settings"
   set | grep -E "http_proxy|https_proxy"
 }
 
