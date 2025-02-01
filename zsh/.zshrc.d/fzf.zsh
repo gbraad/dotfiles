cd7982ded89cb4359e1e8be7dfb07fd274857d7f
Gerard Braad <me@gbraad.nl>
2025-02-01 12:51:46 +0800

    Even more broken fzf package?

--- zsh/.zshrc.d/fzf.zsh
+++ zsh/.zshrc.d/fzf.zsh
@@ -3,5 +3,12 @@
 if [[ -r /usr/share/fzf/shell/key-bindings.zsh ]]; then
     source /usr/share/fzf/shell/key-bindings.zsh
     # since package is b0rked?
-    source /usr/share/zsh/site-functions/fzf
+    if [[ -r /usr/share/zsh/site-functions/fzf ]]; then
+        source /usr/share/zsh/site-functions/fzf
+    fi
+    # even more broken ?
+    if [[ -r /usr/share/zsh/site-functions/_fzf ]]; then
+        source /usr/share/zsh/site-functions/_fzf
+    fi
 fi
+
