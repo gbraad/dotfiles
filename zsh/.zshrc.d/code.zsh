23dbd31d520ebae56fbbb969b8a0b517b0bb7ebe
Gerard Braad <me@gbraad.nl>
2025-02-03 17:47:05 +0800

    Add code-cli helper functions

--- zsh/.zshrc.d/code.zsh
+++ zsh/.zshrc.d/code.zsh
@@ -1,8 +1,57 @@
+#!/bin/zsh
+
+CONFIG="${HOME}/.code"
+alias codeini="git config -f $CONFIG"
+
+_codepath=$(codeini --get code.path) || _codepath="${HOME}/.local/bin"
+_codepath=$(echo ${_codepath} | envsubst)
+
+_codeexists() {
+    if [ -f "${_codepath}/code" ]; then
+        return 0  # File exists, return true (0)
+    else
+        return 1  # File does not exist, return false (1)
+    fi
+}
+
 _installcode() {
+    echo "Installing code-cli"
+    # does not handle architecture yet
+
     tempfile=$(mktemp)
     curl -fsSL "https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64" -o ${tempfile}
-    tar zxvf ${tempfile} -C ${HOME}/.local/bin
+    tar -zxvf ${tempfile} -C ${_codepath} > /dev/null 2>&1
     rm -f ${tempfile}
 }
 
+
+
+_startcodetunnel() {
+    if ! _codeexists; then
+        _installcode
+    fi
+
+    screen "${_codepath}/code tunnel --accept-server-license-terms --name ${HOSTNAME}"
+}
+
+_startcodeserveweb() {
+    if ! _codeexists; then
+        _installcode
+    fi
+
+    screen "${_codepath}/code serve-web --without-connection-token --host 0.0.0.0"
+}
+
 alias install-code="_installcode"
+alias start-code-tunnel="_startcodetunnel"
+alias start-code-serveweb="_startcodeserveweb"
+
+_installcodeifmissing() {
+    if ! _codeexists; then
+        _installcode
+    fi
+}
+
+if [[ $(codeini --get "code.autoinstall") == true ]]; then
+  _installcodeifmissing
+fi
