a1ff115337afe327434af6adc1e08fd32b2a62a6
Gerard Braad <me@gbraad.nl>
2025-02-15 13:37:42 +0000

    Update postStartCommand

--- .github/install.sh
+++ .github/install.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 # clean workspace folder
+rm -rf /workspaces/upstream
+ln -s ~/.dotfiles /workspaces/upstream
+
 rm -rf /workspaces/dotfiles
 ln -s ~/.dotfiles /workspaces/dotfiles
+
+rm -rf /workspaces/dotfiles-downstream
+ln -s ~/.dotfiles /workspaces/dotfiles-downstream
+
 
 cd ~
 
